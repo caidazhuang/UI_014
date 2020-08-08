8ab0ad784be635086842bcc918656273120b67e9
caidazhuang <1696468530@qq.com>
2020-08-08 09:57:09 +0800

    111

--- scripts/test_001.py
+++ scripts/test_001.py
@@ -1,4 +1,10 @@
+import allure
 class Test_001:
+    @allure.step('下单')
     def test_001(self):
         print('111')
+        assert True
+    @allure.step('登录')
+    def test_002(self):
+        print('222')
         assert True
